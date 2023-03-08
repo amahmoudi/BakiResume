109a2e7d34c983be9bbdbca22e062ac318ed361f
abdelbaki <abdelbaki.mahmoudi@gmail.com>
2023-03-08 10:36:23 +0100

    correct same errors

--- src/I18n/locale/fr/locale_fr.js
+++ src/I18n/locale/fr/locale_fr.js
@@ -2,14 +2,14 @@ const locale_fr ={
     "welcome": "Salut tout le monde",
     "iam": "Je suis",
     "nom": "Abdelbaki",
-    "titleProfile": "Ingénieur Devops",
+    "titleProfile": "Ingénieur Full Stack Developer/Devops/Cloud Aws",
     "msgAcc1": "Ravi de vous voir ici!",
-    "msgAcc2": "Je suis un ingénieur en informatique spécialisée dans l'étude et le développement de systèmes informatiques.",
+    "msgAcc2": "Je suis un Lead Developer/DevOps/Cloud Aws.",
     "skill": "Compétences",
     "listProjects": "La liste des projets",
-    "presentation": "Salut, je m'appelle Mahmoudi Abdelbaki. J'ai 34 ans. Je suis un ingénieur en informatique spécialisé  \n" +
-    "dans la conception et le développement de systèmes informatiques. Je travaille actuellement comme ingénieur.  \n" +
-    "Je suis très fier de toute ma carrière, j'ai beaucoup de choses et de rêves à faire dans le futur.  \n" +
+    "presentation": "Salut, je suis Mahmoudi Abdelbaki. J'ai 34 ans. Je suis ingénieur informatique spécialisé  \n" +
+    "dans la conception et le développement des systèmes informatiques. Je travaille actuellement comme Lead Developer/DevOps/Aws Cloud.\n" +
+    "J'aime bien mon métier et j'en suis très fier de toute ma carrière, j'ai beaucoup de projets dont je rêve à réaliser dans le futur.  \n" +
     "Vous voulez me poser une question ?",
     "contactMe": "Contactez moi",
     "contactMeMsg": "Vous pouvez m'appeler, m'envoyer un SMS ou m'envoyer un e-mail s'il y a quelque chose dont vous aimeriez discuter !",
